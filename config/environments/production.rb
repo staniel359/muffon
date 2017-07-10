1f9a5d457a01995a3a35d139bf8c98e37e181386
staniel359 <superninjazombieraptor97@gmail.com>
2017-07-10 13:29:51 +0300

    Add assets settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = false
-  config.assets.compile = false
-  config.assets.digest = true
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
