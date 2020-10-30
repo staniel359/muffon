d7499f48477943347602d216feaa888ed48544c2
staniel359 <staniel359@gmail.com>
2020-10-30 13:32:55 +0300

    Add Artist page Similar + pagination

--- src/components/artists/show/Similar.jsx
+++ src/components/artists/show/Similar.jsx
@@ -1,37 +1,103 @@
 import React from 'react'
 import { v4 as uuid } from 'uuid'
-import { Header, Segment } from 'semantic-ui-react'
+import { Header, Segment, Pagination } from 'semantic-ui-react'
+import axios from 'axios'
 
 export default class Similar extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      loading: true,
+      page: 1
+    }
+  }
+
+  limit = 4
+
+  componentDidMount () {
+    this.getSimilar()
+  }
+
+  getSimilar () {
+    this.setState({ loading: true })
+    axios(this.similarLink()).then(resp => this.setSimilarData(resp))
+  }
+
+  similarLink () {
+    return {
+      method: 'GET',
+      url: `/lastfm/artists/${this.props.artistName}/similar`,
+      params: {
+        limit: this.limit,
+        page: this.state.page
+      }
+    }
+  }
+
+  setSimilarData (resp) {
+    this.setState({
+      similar: resp.data.artist.similar,
+      totalPages: resp.data.artist.total_pages,
+      loading: false
+    })
   }
 
   similarList () {
     return (
       <div>
         {this.state.similar.map(artist => {
-          return (
-            <div key={uuid()}>
-              <div>name: {artist}</div>
-            </div>
-          )
+          return <div key={uuid()}>{artist}</div>
         })}
       </div>
     )
   }
 
+  pagination() {
+    return (
+      <Pagination
+        defaultActivePage={this.state.page}
+        totalPages={this.state.totalPages}
+        firstItem={null}
+        lastItem={null}
+        siblingRange={0}
+        onPageChange={this.handlePageChange}
+      />
+    )
+  }
+
+  handlePageChange = (e, { activePage }) => {
+    this.scrollToSegmentTop()
+    this.setState(
+      { page: activePage },
+      this.getSimilar
+    )
+  }
+
+  scrollToSegmentTop () {
+    window.scrollTo(0, this.segmentTop())
+  }
+
+  segmentTop() {
+    return document.getElementById(
+      'artistPageSimilarSegment'
+    ).offsetTop - 107
+  }
+
   render () {
     return (
       <React.Fragment>
-        <Header as="h4" attached="top" content="Similar" />
+        <Header as="h3" attached="top" content="Similar" />
         <Segment
           className="artistPageSegment"
-          loading={!this.state.similar}
-          attached="bottom"
+          id="artistPageSimilarSegment"
+          loading={this.state.loading}
+          attached
           content={this.state.similar && this.similarList()}
         />
+
+        <Segment className="artistPagePaginationWrap" attached="bottom">
+          {this.state.similar && this.pagination()}
+        </Segment>
       </React.Fragment>
     )
   }
