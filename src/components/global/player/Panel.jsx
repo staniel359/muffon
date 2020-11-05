b077cea586d932a9dfd19d1d197c33e9519536fa
staniel359 <staniel359@gmail.com>
2020-11-05 13:10:50 +0300

    Refactror Player panel

--- src/components/global/player/Panel.jsx
+++ src/components/global/player/Panel.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Container, Sidebar, Segment, List } from 'semantic-ui-react'
-import { HashRouter as Router, Link } from 'react-router-dom'
-import Picture from 'src/components/artists/show/Picture'
+import { Container, Sidebar, Segment } from 'semantic-ui-react'
 import 'styles/global/Player.sass'
 import PlayerContext from 'contexts/PlayerContext'
 import AudioContainer from './audio/AudioContainer'
 import Controls from './audio/Controls'
 import TimeBar from './audio/TimeBar'
+import Track from './Track'
 
 export default class Panel extends React.Component {
   static contextType = PlayerContext
@@ -14,77 +13,18 @@ export default class Panel extends React.Component {
   playerPanelContainer () {
     return (
       <Container className="playerPanelContainer">
-        <div className="playerPanelLeftColumn">{this.trackData()}</div>
+        <div className="playerPanelLeftColumn">
+          <Track />
+        </div>
 
         <div className="playerPanelRightColumn">
           <AudioContainer />
 
-          {this.context.playingNowTrack && this.controlsTimeBarData()}
+          <Controls />
+
+          <TimeBar />
         </div>
       </Container>
-    )
-  }
-
-  trackData () {
-    return (
-      <Router>
-        <List className="playerPanelTrackWrap">
-          <List.Item>
-            <div className="imageWrap">
-              <Picture
-                artistName={this.context.playingNowTrack.artist}
-                dimmer
-              />
-            </div>
-
-            {this.trackDataContent()}
-          </List.Item>
-        </List>
-      </Router>
-    )
-  }
-
-  trackDataContent () {
-    return (
-      <List.Content className="contentWrap">
-        <List.Header as="h4">
-          <Link to={this.trackLink()}>
-            {this.context.playingNowTrack.title}
-          </Link>
-        </List.Header>
-
-        <List.Description>
-          <Link to={this.artistLink()}>
-            {this.context.playingNowTrack.artist}
-          </Link>
-        </List.Description>
-      </List.Content>
-    )
-  }
-
-  trackLink () {
-    return `/artists/${this.artistName()}/tracks/${this.trackTitle()}`
-  }
-
-  artistName () {
-    return encodeURIComponent(this.context.playingNowTrack.artist)
-  }
-
-  trackTitle () {
-    return encodeURIComponent(this.context.playingNowTrack.title)
-  }
-
-  artistLink () {
-    return `/artists/${this.artistName()}`
-  }
-
-  controlsTimeBarData () {
-    return (
-      <React.Fragment>
-        <Controls />
-
-        <TimeBar />
-      </React.Fragment>
     )
   }
 
---
+++ src/components/global/player/Track.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { HashRouter as Router, Link } from 'react-router-dom'
+import { List } from 'semantic-ui-react'
+import Picture from 'src/components/artists/show/Picture'
+import PlayerContext from 'contexts/PlayerContext'
+
+export default class Track extends React.Component {
+  static contextType = PlayerContext
+
+  trackLink () {
+    return `/artists/${this.artistName}/tracks/${this.trackTitle}`
+  }
+
+  artistName = encodeURIComponent(this.context.playingNowTrack.artist)
+  trackTitle = encodeURIComponent(this.context.playingNowTrack.title)
+
+  artistLink () {
+    return `/artists/${this.artistName}`
+  }
+
+  render () {
+    return (
+      <Router>
+        <List className="playerPanelTrackWrap">
+          <List.Item>
+            <div className="imageWrap">
+              <Picture
+                artistName={this.context.playingNowTrack.artist}
+                dimmer
+              />
+            </div>
+
+            <List.Content className="contentWrap">
+              <List.Header as="h4">
+                <Link to={this.trackLink()}>
+                  {this.context.playingNowTrack.title}
+                </Link>
+              </List.Header>
+
+              <List.Description>
+                <Link to={this.artistLink()}>
+                  {this.context.playingNowTrack.artist}
+                </Link>
+              </List.Description>
+            </List.Content>
+          </List.Item>
+        </List>
+      </Router>
+    )
+  }
+}
