4e6c3b68036e848641c106e6c9d663f83f624f5a
staniel359 <staniel359@gmail.com>
2020-12-04 11:52:13 +0300

    Search Album/Artist UI Changes

--- src/components/global/search/tabs/albums/Album.jsx
+++ src/components/global/search/tabs/albums/Album.jsx
@@ -3,34 +3,74 @@ import { List, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 export default class Album extends React.PureComponent {
-  render () {
-    const { hideSearch, album } = this.props
+  constructor (props) {
+    super(props)
+    this.state = { active: false, artistHovered: false }
+  }
 
-    const artistName = album.artist
-    const artistNameEncoded = encodeURIComponent(artistName)
-    const albumTitle = album.title
-    const albumTitleEncoded = encodeURIComponent(albumTitle)
-    const albumLink = `/artists/${artistNameEncoded}/albums/${albumTitleEncoded}`
+  render () {
+    const { album, hideSearch } = this.props
+    const { active, artistHovered } = this.state
+
+    const artistNameEncoded = encodeURIComponent(album.artist)
+    const artistPageLink = `/artists/${artistNameEncoded}`
+
+    const albumTitleEncoded = encodeURIComponent(album.title)
+    const albumPageLink = `/artists/${artistNameEncoded}/albums/${albumTitleEncoded}`
+
+    const albumOrArtistLink = artistHovered ? artistPageLink : albumPageLink
+
+    const toggleActive = bool => this.setState({ active: !active })
 
     const coverDefault =
       'https://lastfm.freetls.fastly.net/i/u/64s/' +
       'c6f59c1e5e7240a4c0d427abd71f3dbb.png'
     const cover = album.cover || coverDefault
+    const imageData = (
+      <Image rounded className="searchItemAlbumCover" src={cover} />
+    )
+
+    const albumTitleClassName = active && !artistHovered ? 'colorBase' : ''
+    const albumTitleData = (
+      <List.Header
+        as="h4"
+        className={albumTitleClassName}
+        content={album.title}
+      />
+    )
+
+    const artistNameClassName = artistHovered ? 'colorBase' : ''
+    const toggleArtistHovered = bool =>
+      this.setState({ artistHovered: !artistHovered })
+    const artistNameData = (
+      <List.Description
+        className={artistNameClassName}
+        content={album.artist}
+        onMouseEnter={toggleArtistHovered}
+        onMouseLeave={toggleArtistHovered}
+      />
+    )
+
+    const contentData = (
+      <React.Fragment>
+        {imageData}
+        <List.Content className="searchItemAlbumContent">
+          {albumTitleData}
+          {artistNameData}
+        </List.Content>
+      </React.Fragment>
+    )
 
     return (
       <List.Item
         className="searchItem"
         as={Link}
-        to={albumLink}
+        to={albumOrArtistLink}
         onClick={hideSearch}
-      >
-        <Image rounded className="searchItemAlbumCover" src={cover} />
-
-        <List.Content className="searchItemContent">
-          <List.Header as="h4" content={albumTitle} />
-          <List.Description content={artistName} />
-        </List.Content>
-      </List.Item>
+        onMouseEnter={toggleActive}
+        onMouseLeave={toggleActive}
+        content={contentData}
+      />
     )
   }
 }
--- src/components/global/search/tabs/artists/Artist.jsx
+++ src/components/global/search/tabs/artists/Artist.jsx
@@ -4,14 +4,50 @@ import { Link } from 'react-router-dom'
 import Picture from 'global/artists/Picture'
 
 export default class Artist extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = { active: false }
+  }
+
   render () {
     const { hideSearch, artist } = this.props
+    const { active } = this.state
 
     const artistName = artist.name
     const artistNameEncoded = encodeURIComponent(artistName)
     const artistLink = `/artists/${artistNameEncoded}`
 
+    const toggleActive = bool => this.setState({ active: !active })
+
+    const imageData = (
+      <div className="searchItemImage">
+        <Picture circular size="extrasmall" {...{ artistName }} />
+      </div>
+    )
+
+    const artistNameClassName = active ? 'colorBase' : ''
+    const artistNameData = (
+      <List.Header
+        as="h4"
+        className={artistNameClassName}
+        content={artistName}
+      />
+    )
+
     const listenersCount = artist.listeners_count.toLocaleString('eu')
+    const listenersCountData = (
+      <List.Description content={`${listenersCount} listeners`} />
+    )
+
+    const contentData = (
+      <React.Fragment>
+        {imageData}
+        <List.Content className="searchItemContent">
+          {artistNameData}
+          {listenersCountData}
+        </List.Content>
+      </React.Fragment>
+    )
 
     return (
       <List.Item
@@ -19,16 +55,10 @@ export default class Artist extends React.PureComponent {
         as={Link}
         to={artistLink}
         onClick={hideSearch}
-      >
-        <div className="searchItemImage">
-          <Picture circular size="extrasmall" {...{ artistName }} />
-        </div>
-
-        <List.Content className="searchItemContent">
-          <List.Header as="h4" content={artistName} />
-          <List.Description content={`${listenersCount} listeners`} />
-        </List.Content>
-      </List.Item>
+        onMouseEnter={toggleActive}
+        onMouseLeave={toggleActive}
+        content={contentData}
+      />
     )
   }
 }
