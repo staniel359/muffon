dfc726bace4153f0c9c4be274e921b69c9e1cf40
staniel359 <staniel359@gmail.com>
2021-09-09 15:22:26 +0300

    Remove Sentry

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
-import { init as setSentry } from '#/plugins/sentry'
 
 const isDevelopment =
   process.env.NODE_ENV === 'development'
@@ -31,7 +30,3 @@ app
   .use(store)
   .use(i18n)
   .mount('#app')
-
-if (!isDevelopment) {
-  setSentry(app)
-}
