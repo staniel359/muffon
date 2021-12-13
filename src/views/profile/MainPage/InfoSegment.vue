56128436e71102e576ff032edfbfebf20cf0c76a
staniel359 <staniel359@gmail.com>
2021-12-13 18:14:51 +0300

    Add follow button

--- src/views/profile/MainPage/InfoSegment.vue
+++ src/views/profile/MainPage/InfoSegment.vue
@@ -9,6 +9,11 @@
     <ExtraInfoSection
       :profileData="profileData"
     />
+
+    <FollowSection
+      v-if="isRenderFollowSection"
+      :profileData="profileData"
+    />
   </BaseSegmentContainer>
 </template>
 
@@ -17,16 +22,29 @@ import BaseSegmentContainer
   from '@/containers/segments/BaseSegmentContainer.vue'
 import MainInfoSection from './InfoSegment/MainInfoSection.vue'
 import ExtraInfoSection from './InfoSegment/ExtraInfoSection.vue'
+import FollowSection from './InfoSegment/FollowSection.vue'
+import { isCurrentProfile } from '#/utils'
 
 export default {
   name: 'InfoSegment',
   components: {
     BaseSegmentContainer,
     MainInfoSection,
-    ExtraInfoSection
+    ExtraInfoSection,
+    FollowSection
   },
   props: {
     profileData: Object
+  },
+  computed: {
+    isRenderFollowSection () {
+      return !isCurrentProfile(
+        this.profileId
+      )
+    },
+    profileId () {
+      return this.profileData.id
+    }
   }
 }
 </script>
