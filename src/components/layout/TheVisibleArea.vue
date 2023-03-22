f1ace0b3c2e67f3e0de0778c393d9094f41b5b5a
staniel359 <staniel359@gmail.com>
2023-03-22 15:00:28 +0300

    Add player panel visibility logic to visible area

--- src/components/layout/TheVisibleArea.vue
+++ src/components/layout/TheVisibleArea.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="the-visible-area-container">
+  <div
+    class="the-visible-area-container"
+    :class="{
+      'player-panel-height': playerPlaying
+    }"
+  >
     <div
       ref="visible"
       class="the-visible-area"
@@ -9,12 +14,22 @@
 
 <script>
 import {
+  mapState,
   mapActions
 } from 'pinia'
 import layoutStore from '@/stores/layout'
+import playerStore from '@/stores/player'
 
 export default {
   name: 'TheVisibleArea',
+  computed: {
+    ...mapState(
+      playerStore,
+      {
+        playerPlaying: 'playing'
+      }
+    )
+  },
   mounted () {
     this.setVisibleContext(
       this.$refs.visible
@@ -34,9 +49,11 @@ export default {
 <style lang="sass" scoped>
 .the-visible-area-container
   @extend .w-100, .fixed, .pointer-events-none
-  height: calc(100vh - #{$navbarHeight} - #{$playerPanelHeight})
+  height: calc(100vh - #{$navbarHeight})
   top: $navbarHeight
   z-index: 10000
+  &.player-panel-height
+    height: calc(100vh - #{$navbarHeight} - #{$playerPanelHeight})
 
 .the-visible-area
   @extend .w-100, .h-100, .relative
