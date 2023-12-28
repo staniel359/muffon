98e98feb3f35b05909162779a545964b23f741d9
Xyloflake <xyloflake@gmail.com>
2023-12-29 02:00:47 +0530

    Fix 5.1/4.0 audio bug (#129)

--- electron/actions/app/setFlags.js
+++ electron/actions/app/setFlags.js
@@ -6,12 +6,6 @@ export default function () {
   app
     .commandLine
     .appendSwitch(
-      'try-supported-channel-layouts'
-    )
-
-  app
-    .commandLine
-    .appendSwitch(
       'disable-http-cache'
     )
 }
