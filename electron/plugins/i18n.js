42a04f896993802930a662e69843a4959cfe3528
staniel359 <staniel359@gmail.com>
2024-05-17 10:15:31 +0300

    Update dependencies

--- electron/plugins/i18n.js
+++ electron/plugins/i18n.js
@@ -1,13 +1,13 @@
 import i18n from 'i18n'
 import getElectronStoreKey from '../actions/electronStore/getKey.js'
-import be from './i18n/locales/be.json' assert { type: 'json' }
-import de from './i18n/locales/de.json' assert { type: 'json' }
-import en from './i18n/locales/en.json' assert { type: 'json' }
-import fr from './i18n/locales/fr.json' assert { type: 'json' }
-import he from './i18n/locales/he.json' assert { type: 'json' }
-import it from './i18n/locales/it.json' assert { type: 'json' }
-import ja from './i18n/locales/ja.json' assert { type: 'json' }
-import ru from './i18n/locales/ru.json' assert { type: 'json' }
+import be from './i18n/locales/be.json' with { type: 'json' }
+import de from './i18n/locales/de.json' with { type: 'json' }
+import en from './i18n/locales/en.json' with { type: 'json' }
+import fr from './i18n/locales/fr.json' with { type: 'json' }
+import he from './i18n/locales/he.json' with { type: 'json' }
+import it from './i18n/locales/it.json' with { type: 'json' }
+import ja from './i18n/locales/ja.json' with { type: 'json' }
+import ru from './i18n/locales/ru.json' with { type: 'json' }
 
 // i18n
 
