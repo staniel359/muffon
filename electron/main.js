c90006d98dc55ef3d7d73af887815b078595951d
staniel359 <staniel359@gmail.com>
2024-06-18 00:10:40 +0300

    Change second instance handler from `quit` to `exit`

--- electron/main.js
+++ electron/main.js
@@ -6,11 +6,12 @@ import {
   handleHarmfulSwitches
 } from './helpers/utils.js'
 import initialize from './actions/app/initialize.js'
+import exit from './actions/app/exit.js'
 
 handleHarmfulSwitches()
 
 if (isSingleInstance) {
   initialize()
 } else {
-  app.quit()
+  exit()
 }
