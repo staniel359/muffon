6fca42a2b512e33d0c1cad46ec19bb46324e5b54
staniel359 <dark.pizza.42@gmail.com>
2025-08-12 00:16:52 +0300

    change: add role label to profile posts/comments

---
+++ src/components/labels/profile/BaseProfileRoleLabel.vue
@@ -0,0 +1,57 @@
+<template>
+  <BaseLabel
+    v-if="isRender"
+    :class="[
+      'primary circular',
+      {
+        'small': isSmall,
+        'main-right-mini-section': isRight
+      }
+    ]"
+    :text="roleText"
+    :is-invertable="false"
+  />
+</template>
+
+<script>
+import BaseLabel from '@/components/labels/BaseLabel.vue'
+
+export default {
+  name: 'BaseProfileRoleLabel',
+  components: {
+    BaseLabel
+  },
+  props: {
+    profileData: {
+      type: Object,
+      required: true
+    },
+    isSmall: Boolean,
+    isRight: Boolean
+  },
+  data () {
+    return {
+      roles: [
+        'creator'
+      ]
+    }
+  },
+  computed: {
+    isRender () {
+      return this.roles.includes(
+        this.role
+      )
+    },
+    role () {
+      return this.profileData.role
+    },
+    roleText () {
+      return this.$t(
+        `roles.${this.role}`
+      )
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
