34e40ed3161702be02a29221845f686b642c4799
staniel359 <dark.pizza.42@gmail.com>
2025-12-06 12:45:24 +0300

    fix: incorrect window size detection on Linux

--- electron/actions/mainWindow/getSize.js
+++ electron/actions/mainWindow/getSize.js
@@ -1,26 +1,8 @@
-import {
-  isLinux
-} from '../../helpers/utils.js'
-
 export default function () {
-  let width
-
-  let height
-
-  if (isLinux) {
-    const size = mainWindow.getSize()
-
-    width = size[0]
-
-    height = size[1]
-  } else {
-    const size =
-      mainWindow.getContentSize()
-
-    width = size[0]
-
-    height = size[1]
-  }
+  const [
+    width,
+    height
+  ] = mainWindow.getContentSize()
 
   return [
     width,
