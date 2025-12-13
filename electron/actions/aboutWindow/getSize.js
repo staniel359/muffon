33f7d6b6721d7d2f7cdd943af97a693530b475c9
staniel359 <dark.pizza.42@gmail.com>
2025-12-13 22:47:18 +0300

    fix: bug with incorrect bounds of about window

--- electron/actions/aboutWindow/getSize.js
+++ electron/actions/aboutWindow/getSize.js
@@ -1,26 +1,8 @@
-import {
-  isLinux
-} from '../../helpers/utils.js'
-
 export default function () {
-  let width
-
-  let height
-
-  if (isLinux) {
-    const size = aboutWindow.getSize()
-
-    width = size[0]
-
-    height = size[1]
-  } else {
-    const size =
-      aboutWindow.getContentBounds()
-
-    width = size.width
-
-    height = size.height
-  }
+  const [
+    width,
+    height
+  ] = aboutWindow.getContentSize()
 
   return [
     width,
