2ba6644988ab414b052d671127b626101ea2b454
staniel359 <dark.pizza.42@gmail.com>
2026-01-11 01:45:16 +0300

    fix: youtube player errors

--- electron/actions/session/headers/addHandlers.js
+++ electron/actions/session/headers/addHandlers.js
@@ -1,8 +1,83 @@
-import addYouTubeHeadersHandler from './youtube/addHandler.js'
-import addYouTubeMusicHeadersHandler from './youtubeMusic/addHandler.js'
+import {
+  session
+} from 'electron'
+
+const filter = {
+  urls: [
+    'https://www.youtube.com/*',
+    'https://lh3.googleusercontent.com/*'
+  ]
+}
+
+function handleBeforeSendHeaders (
+  details,
+  callback
+) {
+  const {
+    url
+  } = details
+
+  const headers = {
+    ...details.responseHeaders
+  }
+
+  if (url.includes(
+    'youtube.com'
+  )) {
+    headers.Referer = 'https://www.example.com'
+  } else if (url.includes(
+    'lh3.googleusercontent.com'
+  )) {
+    headers.Referer = 'https://music.youtube.com/'
+  }
+
+  callback(
+    {
+      cancel: false,
+      requestHeaders: headers
+    }
+  )
+}
+
+function handleHeadersReceived (
+  details,
+  callback
+) {
+  const {
+    url
+  } = details
+
+  const headers = {
+    ...details.responseHeaders
+  }
+
+  if (url.includes(
+    'youtube.com'
+  )) {
+    headers['Access-Control-Allow-Origin'] = [
+      '*'
+    ]
+  }
+
+  callback(
+    {
+      responseHeaders: headers
+    }
+  )
+}
 
 export default function () {
-  addYouTubeHeadersHandler()
+  const {
+    webRequest
+  } = session.defaultSession
 
-  addYouTubeMusicHeadersHandler()
+  webRequest.onBeforeSendHeaders(
+    filter,
+    handleBeforeSendHeaders
+  )
+
+  webRequest.onHeadersReceived(
+    filter,
+    handleHeadersReceived
+  )
 }
